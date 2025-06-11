d7de9b39cc498073616311940fc13e0b61113a97
lislis <mail@lislis.de>
2025-06-11 17:11:41 +0200

    add simple nodejs osc monitor

---
+++ code/environments/nodejs/index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+import { Server } from 'node-osc';
+
+const args = process.argv.slice(2);
+
+const port = args[0] ?  process.argv[0] : 3333;
+
+var oscServer = new Server(port, '0.0.0.0', () => {
+  console.log(`OSC Server is listening on port ${port}`);
+});
+
+oscServer.on('message', function (msg) {
+  console.log(`Message: ${msg}`);
+  //oscServer.close();
+});
